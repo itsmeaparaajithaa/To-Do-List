27bd7f44f3b16f0d7bb51ab41a241b6fbf36c119
Aparaajithaa Sree G <141312183+itsmeaparaajithaa@users.noreply.github.com>
2024-02-04 12:31:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+'use strict';
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+function addTask(){
+    if(inputBox.value === ''){
+        alert('Please Enter some task');
+    }
+    else{
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span)  
+    }
+inputBox.value = '';
+saveData();
+}
+listContainer.addEventListener('click',function(e){
+    console.log(e);
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        saveData();
+    }
+    else{
+        e.target.parentElement.remove();
+        saveData();
+    }
+}
+,false);
+
+document.addEventListener('keydown', function(e){
+    if(e.key==='Enter'){
+        addTask();
+    }
+})
+function saveData(){
+    localStorage.setItem('data',listContainer.innerHTML)
+}
+
+function displayData(){
+    listContainer.innerHTML=localStorage.getItem('data');
+}
+
+displayData();
